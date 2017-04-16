be9e68f3db53b82b79cce202170eead89c9cc02b
Yassine Doghri <doghri.yassine@gmail.com>
2017-04-16 18:40:19 +0800

    Comment to explain weather API not working properly

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,7 +16,7 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // get all crisis
-		$data = $this->getDoctrine()->getManager()->getRepository('AppBundle:Crisis')->findAll();
+        $data = $this->getDoctrine()->getManager()->getRepository('AppBundle:Crisis')->findAll();
         $haze = $this->getDoctrine()->getManager()->getRepository('AppBundle:Haze')->findAll();
         dump($haze);
         dump($data);
@@ -24,14 +24,15 @@ class DefaultController extends Controller
         return array(
             'data' => $data,
             'weatherData' => $weatherData,
-			'hazeInfo' => $haze
-            );
+            'hazeInfo' => $haze
+        );
     }
 
     //$url = "https://api.data.gov.sg/v1/environment/24-hour-weather-forecast?date=2017-04-08"
 
-    public function getWeatherData(){
-    	date_default_timezone_set("Asia/Singapore");
+    public function getWeatherData()
+    {
+        date_default_timezone_set("Asia/Singapore");
         //curl logic
         $ch = curl_init();
         $header = array('api-key: hUGj5N4dlvebrVDN7vGGaiWrffRPoUlu',);
@@ -40,18 +41,22 @@ class DefaultController extends Controller
         $hour = date("H");
         $minute = date("i");
         $second = date("s");
-        
+
+
+        // The api from data.gov.sg is unstable and does not work on some days.
+        // An alternate static link is given below to demonstrate the working api.
+
         //$url = "https://api.data.gov.sg/v1/environment/24-hour-weather-forecast?date_time=" . $date . "T" . $hour . "%3A" . $minute . "%3A" . $second;
         $url = "https://api.data.gov.sg/v1/environment/24-hour-weather-forecast?date=2017-04-07";
 
-        curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
         curl_setopt($ch, CURLOPT_HEADER, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 
-        $weatherDataResponse= curl_exec ($ch);
+        $weatherDataResponse = curl_exec($ch);
         curl_close($ch);
         $weatherData = json_decode(strstr($weatherDataResponse, "{"), true);
         return $weatherData;
